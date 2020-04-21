da7efc2b60b28e0b058795d2c4471474f3b20233
Felipe Esdras Navarro <fesnavarro@users.noreply.github.com>
2020-04-21 15:22:34 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,20 +8,21 @@ sudo sed -i -e 's/quiet splash//g' /etc/default/grub
 update-grub
 
 echo "### System update"
-sudo pacman -Syu
+sudo apt update && sudo apt autoremove -y && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoclean
+
 
 echo "### Install default programs"
-sudo pacman -S yay
-sudo pacman -Sy snapd
+sudo apt install snapd -y
 sudo systemctl enable snapd.service
 sudo systemctl start snapd.service
-sudo pacman -Sy jdk-openjdk
+sudo apt install jdk-openjdk -y
 
-sudo pacman -Sy openssh
+sudo apt install openssh -y
 sudo systemctl enable sshd.service
 sudo systemctl start sshd.service
 
-sudo pacman -Sy dstat
+sudo apt install dstat -y
+sudo apt install htop -y
 
 sudo modprobe zram
 sudo sh -c "echo 'lz4' > /sys/block/zram0/comp_algorithm"
@@ -31,54 +32,49 @@ sudo swapon --priority 100 /dev/zram0
 sudo cat /tmp/desktop-bootstrap/files/zram.service > /etc/systemd/system/zram.service
 sudo systemctl enable zram
 
-sudo pacman -Sy flatpak
-
+sudo apt install flatpak -y
+ 
 echo "### Docker"
-sudo pacman -Sy docker
-sudo pacman -Sy docker-compose
+sudo apt install docker -y
+sudo apt install docker-compose -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
 echo "### freeOffice"
-sudo pacman -Sy freeoffice
+sudo apt install freeoffice -y
 
 #Calculadora cientifica
 echo  "### SpeedCrunch CALC"
-sudo pacman -Sy speedcrunch
+sudo apt install speedcrunch -y
 
 
-echo "### ATOM"
-sudo pacman -Sy atom
+echo "### VSCODE"
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
 
 echo "### ARDUINO"
-sudo pacman -Sy arduino
-
-echo "### Install nextcloud"
-sudo pacman -Sy nextcloud-client
+sudo apt install arduino -y
 
 echo "### Install keepassXC"
-sudo pacman -Sy keepassxc
+snap install keepassxc
 
 echo "### Install Acess programs"
-sudo pacman -Sy vnc-viewer
-yay teamviewer #option 1
-systemctl enable teamviewerd
-yay anydesk #option 1
-sudo pacman -Sy remmina
+sudo apt install vnc-viewer -y
+sudo apt install teamviewer -y
+sudo apt install  anydesk -y
+sudo apt install remmina -y
 
 echo "### Install graphic editors"
-sudo pacman -Sy gimp
-sudo pacman -Sy inkscape
-sudo pacman -Sy kdenlive
-sudo pacman -Sy blender
-sudo pacman -Sy obs-studio
+sudo apt install gimp
+sudo apt install inkscape
+sudo apt install blender
+sudo apt install obs-studio
 snap install opentoonz
 
 echo "### Install audio editor"
 snap install audacity
-
-echo "### Enable monitor detect"
-optirun intel-virtual-output
 
 echo "Pronto você já pode aproveitar dos seus novos recursos instalados!"
 bash -x ./tmp/desktop-bootstrap/complementos/bat.sh
