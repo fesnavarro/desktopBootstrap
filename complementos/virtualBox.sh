79200702bdf7e4f2d652d4240501c5631d08dad5
Felipe Esdras Navarro <fesnavarro@users.noreply.github.com>
2020-04-23 01:24:10 +1000

    Update virtualBox.sh

--- complementos/virtualBox.sh
+++ complementos/virtualBox.sh
@@ -1,5 +1,5 @@
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian bionic contrib"
-sudo apt install virtualbox-6.0
+sudo apt install virtualbox-6.1
 sudo usermod -aG vboxusers $USER
